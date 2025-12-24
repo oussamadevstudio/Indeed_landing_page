b6ec1c0f40a1d614816b76194014129f81cb4f5c
oussamadev.studio <oussamadev.studio@gmail.com>
2025-12-24 12:30:41 +0100

    Editing Layout of NormalGrid

--- src/components/ui/Normalgridcard.jsx
+++ src/components/ui/Normalgridcard.jsx
@@ -1,17 +1,19 @@
+import { CiStar } from "react-icons/ci";
+
 const Normalgridcard = ({cityAbv, cityName, numberOfSalariesReported, SalaryPerHour}) => {
 
     return(
         <>
-            <div className="w-63 h-37 border-2 border-black">
-                <div className="flex ">
+            <div className="w-63 h-37 rounded-xl p-6 bg-cyan-50">
+                <div className="flex gap-x-1">
                     <div className="flex justify-center items-center w-11 h-11 rounded-full bg-blue-800 ">{cityAbv}</div>
                     <div className="">
                         <h3>{cityName}</h3>
-                        <p>{numberOfSalariesReported}{" "}Salaries reported</p>
+                        <div className="flex items-center"><CiStar /><p>{numberOfSalariesReported}{" "}Salaries reported</p></div>
                     </div>
                 </div>
                 <hr />
-                <div className="">
+                <div className="my-4">
                     <h3>{SalaryPerHour}</h3>
                 </div>
             </div>
